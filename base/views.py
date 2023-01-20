b1aa0f725ca576636c3022169e7e2fc335557811
Vilsivul <92055005+VLadislav-Davidenko@users.noreply.github.com>
2023-01-20 09:56:34 +0200

    SideBar + SearchBar + Cleaning

--- base/views.py
+++ base/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
-from .models import Room
+from django.db.models import Q
+from .models import Room, Topic
 from .forms import RoomForm
 
 # rooms = [
@@ -9,8 +10,16 @@ from .forms import RoomForm
 # ]
 
 def home(request):
-    rooms = Room.objects.all()
-    context = {"rooms": rooms}
+    q = request.GET.get('q') if request.GET.get('q') != None else ''
+    rooms = Room.objects.filter(
+        Q(topic__name__icontains=q) |
+        Q(name__icontains=q) |
+        Q(description__icontains=q)
+        )
+
+    rooms_count = rooms.count()
+    topics = Topic.objects.all()
+    context = {"rooms": rooms, "topics": topics, "rooms_count": rooms_count}
     return render(request, "base/home.html", context)
 
 
@@ -18,6 +27,7 @@ def room(request, pk):
     room = Room.objects.get(id=pk)
     context = {"room": room}
     return render(request, "base/room.html", context)
+
 
 def createRoom(request):
     form = RoomForm()
@@ -31,6 +41,7 @@ def createRoom(request):
     context = {"form": form}
     return render(request, "base/room_form.html", context)
 
+
 def updateRoom(request, pk):
     room = Room.objects.get(id=pk)
     form = RoomForm(instance=room)
@@ -43,6 +54,7 @@ def updateRoom(request, pk):
 
     context = {"form": form}
     return render(request, "base/room_form.html", context)
+
 
 def deleteRoom(request, pk):
     room = Room.objects.get(id=pk)
